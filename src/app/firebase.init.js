0244be7adbb5dc10bedd3841fda232d0db3e0831
abdurrashidkhan <a.rashidkha.dev@gmail.com>
2026-01-22 01:04:20 +0600

    new hosting testing

--- src/app/firebase.init.js
+++ src/app/firebase.init.js
@@ -5,15 +5,16 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDx7GBeQhYcS0lJB2WaF_qmrLBVL2Dy4-M",
-  authDomain: "e-bazar-8bfa2.firebaseapp.com",
-  projectId: "e-bazar-8bfa2",
-  storageBucket: "e-bazar-8bfa2.firebasestorage.app",
-  messagingSenderId: "417987871053",
-  appId: "1:417987871053:web:ed3987248ca16509e8f704"
+  apiKey: "AIzaSyCmr1WH3-nlVHHQDoXkEPDALR11rdNjTF0",
+  authDomain: "e-bazars.firebaseapp.com",
+  projectId: "e-bazars",
+  storageBucket: "e-bazars.firebasestorage.app",
+  messagingSenderId: "996984238541",
+  appId: "1:996984238541:web:4e579741cd2829655d5484",
+  measurementId: "G-DNT7YQNG30",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
