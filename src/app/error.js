25dc3e675d62ae6ffdd6343e145440cc06130fa2
abdurrashidkhan <a.rashidkhan.dev.com>
2026-05-19 16:19:28 +0600

    error massage show and reset button added

--- src/app/error.js
+++ src/app/error.js
@@ -1,10 +1,23 @@
-"use client"
+'use client';
 export default function Error({ error, reset }) {
+  console.log(`error `, error);
   return (
-    <div className="absolute top-[50%] left-[45%] text-center">
-      <h1>{error}</h1>
-      {/* <p>{error}</p> */}
-      <button onClick={() => reset()}>Retry</button>
-    </div>
-  )
+    <section className='container mx-auto px-2 py-10'>
+      <div className=' min-h-screen relative'>
+        <div className=' shadow-2xl border bg-[#f1f1f1] rounded' id='content_center'>
+          <div className='py-5 px-5 text-center'>
+            <h2 className='text-2xl'>Something went wrong!</h2>
+            {/* WRONG: <p>{error}</p> */}
+            <p className='py-2'>{error.message}</p> {/* RIGHT */}
+            <button
+              className='bg-red-800 px-4 py-2 rounded text-white hover:bg-red-900 ease-in-out duration-500'
+              onClick={() => reset()}
+            >
+              Try again
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
